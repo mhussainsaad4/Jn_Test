27839c58d2d909ee9341adcf99f633a9f180cee3
Hussain Saad <mhussainsaad4@gmail.com>
2022-05-15 17:26:01 +0500

    architecture update

---
+++ app/src/main/java/com/example/jntest/utils/K.kt
@@ -0,0 +1,13 @@
+package com.example.jntest.utils
+
+class K {               //constants file
+    class Constants {
+        companion object {
+        }
+    }
+
+    class BundleConstants {
+        companion object {
+        }
+    }
+}
