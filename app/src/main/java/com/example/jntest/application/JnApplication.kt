5965fb1256211c57078c686caefd9e253ee6ac2e
Hussain Saad <mhussainsaad4@gmail.com>
2022-05-15 18:17:29 +0500

    home databinding update

--- app/src/main/java/com/example/jntest/application/JnApplication.kt
+++ app/src/main/java/com/example/jntest/application/JnApplication.kt
@@ -2,7 +2,9 @@ package com.example.jntest.application
 
 import android.app.Application
 import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class JnApplication : Application() {
     companion object {
         lateinit var appContext: Context
--- app/src/main/java/com/example/jntest/home/HomeActivity.kt
+++ app/src/main/java/com/example/jntest/home/HomeActivity.kt
@@ -2,13 +2,31 @@ package com.example.jntest.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.jntest.R
+import com.example.jntest.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        initCode()
+    }
+
+    private fun initCode() {
+        binding.lifecycleOwner = this@HomeActivity
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            binding.navHostFragment.id
+        ) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
